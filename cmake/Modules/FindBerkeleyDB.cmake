845f700376db85e1b3ed682b1d1b6ba3bb7e6fcc
danta <lodemertens@hotmail.com>
2013-03-27 18:23:49 +0100

    do not force the user to have both bdb and kc
    Change the build files to only build the db libraries that are actually
    present on the system. So if you don't have Berkeley db, we will not try
    to make the library for Berkeley db.
    
    Also added the same override flags for Kyoto Cabinet as we had for
    Berkeley DB. This way you can link to any version you want and are not
    forced to use the system default.

--- cmake/Modules/FindBerkeleyDB.cmake
+++ cmake/Modules/FindBerkeleyDB.cmake
@@ -1,10 +1,6 @@
 # -*- cmake -*-
-
-set(DB_FIND_QUIETLY ON)
-set(DB_FIND_REQUIRED ON)
-
-if (NOT DB_INCLUDE_DIR)
-  find_path(DB_INCLUDE_DIR
+if (NOT BerkeleyDB_INCLUDE_DIR)
+  find_path(BerkeleyDB_INCLUDE_DIR
     NAMES
       db.h
     PATHS
@@ -13,10 +9,10 @@ if (NOT DB_INCLUDE_DIR)
       /opt/local/include
       /sw/include
   )
-endif (NOT DB_INCLUDE_DIR)
+endif (NOT BerkeleyDB_INCLUDE_DIR)
 
-if (NOT DB_LIBRARY)
-  find_library(DB_LIBRARY
+if (NOT BerkeleyDB_LIBRARY)
+  find_library(BerkeleyDB_LIBRARY
     NAMES
       db
     PATHS
@@ -25,8 +21,15 @@ if (NOT DB_LIBRARY)
       /opt/local/lib
       /sw/lib
   )
-endif (NOT DB_LIBRARY)
+endif (NOT BerkeleyDB_LIBRARY)
 
-if (NOT DB_LIBRARY)
+if(BerkeleyDB_INCLUDE_DIR AND BerkeleyDB_LIBRARY)
+  set(BerkeleyDB_FOUND TRUE)
+  if(NOT BerkeleyDB_FIND_QUIETLY)
+	message(STATUS "Found Berkeley DB: ${BerkeleyDB_LIBRARY}")
+  endif(NOT BerkeleyDB_FIND_QUIETLY)
+endif(BerkeleyDB_INCLUDE_DIR AND BerkeleyDB_LIBRARY)
+
+if (NOT BerkeleyDB_FOUND AND KyotoCabinet_FIND_REQUIRED)
   message(FATAL_ERROR "Could not find Berkeley DB")
-endif (NOT DB_LIBRARY)
+endif (NOT BerkeleyDB_FOUND AND KyotoCabinet_FIND_REQUIRED)
